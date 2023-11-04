ba58232a67fbfc5778278c229372886a604d394a
shvass <akshayb@gmx.com>
2023-11-04 21:40:34 +0530

    added docs for driver

--- main/driver.hpp
+++ main/driver.hpp
@@ -5,42 +5,84 @@
 #include <driver/mcpwm_prelude.h>
 
 
-#define MOTOR_MIN_PULSEWIDTH_US 10  // Minimum pulse width in microsecond
-#define MOTOR_MAX_PULSEWIDTH_US 19000
-#define MOTOR_TIMEBASE_RESOLUTION_HZ 1 * 1000 * 1000  // 1MHz, 1us per tick
-#define MOTOR_TIMEBASE_PERIOD        20000
+/**
+ * @brief motor specific configurations
+ */
+#define MOTOR_MIN_PULSEWIDTH_US 10      // Minimum pulse width in microseconds 
+#define MOTOR_MAX_PULSEWIDTH_US 19000   // maximum pulse width in microseconds 
+#define MOTOR_TIMEBASE_RESOLUTION_HZ 1 * 1000 * 1000   // clock frequency in Hertz  ( 1 MHz current) 
+#define MOTOR_TIMEBASE_PERIOD        20000      // PWM time period in clock ticks   ( 20 ms current) 
 
 
 
 
-// two channel motor driver
+/**
+ * @brief driver - two channel pwm generator optimized for driving motors
+ * 
+ * @details Two channel pwm generator optimized such that it uses single timer
+ *  to compare and driver two pwm signal simulataneously.
+ */
 class driver{
-
-    private:
-
-
-
 
     public:
 
-    mcpwm_timer_handle_t timer;
-    mcpwm_oper_handle_t lop = nullptr, rop = nullptr;
-    mcpwm_cmpr_handle_t lcmp = nullptr, rcmp = nullptr;
-    mcpwm_gen_handle_t lgen = nullptr, rgen = nullptr;
-
-
-
+    /**
+     * @brief configures peripherals to generate pwm at mentioned GPIOs.
+     * 
+     * @param left  GPIO pin for left pwm output
+     * @param right  GPIO pin for right pwm output
+     */
     driver(int left, int right);
 
 
+    /**
+     * @brief mapping function to map normalized input (0 to 1) to ( MIN - MAX).
+     * 
+     * @param norm normalized float input strictly between 0.0f to 1.0f 
+     * @return int - pwm uptime duration in terms of clock ticks.
+     */
     static int map(float norm){
         return (norm) * (MOTOR_MAX_PULSEWIDTH_US - MOTOR_MIN_PULSEWIDTH_US) + MOTOR_MIN_PULSEWIDTH_US;
     }
 
+
+    /**
+     * @brief set left and right motor speeds simulatneously
+     * 
+     * @param left noramalized left motor speed
+     * @param right noramalized right motor speed
+     */
     void set(float left = -1, float right = -1);
 
+    /**
+     * @brief Set the Left motor speed
+     * 
+     * @param left noramalized left motor speed
+     */
     void setLeft(float left);
+
+    /**
+     * @brief Set the Left motor speed
+     * 
+     * @param left noramalized left motor speed
+     */
     void setRight(float right);
+
+
+
+    private:
+
+    // mcpwm handlers for internal use  
+
+    /// @brief  pcmwm timers
+    mcpwm_timer_handle_t timer;
+
+    /// @brief mcpwm operators
+    mcpwm_oper_handle_t lop = nullptr, rop = nullptr;
+    /// @brief mcpwm comparators
+    mcpwm_cmpr_handle_t lcmp = nullptr, rcmp = nullptr;
+    /// @brief mcpwm generators
+    mcpwm_gen_handle_t lgen = nullptr, rgen = nullptr;
 
 };
 
